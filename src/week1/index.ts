617b015603f3bc4fcc69f369c05228e3b8bf1c6d
Jongwon Park <pjw970220@gmail.com>
2023-02-09 20:01:45 +0900

    add jsdoc description data type

--- src/week1/index.ts
+++ src/week1/index.ts
@@ -14,7 +14,7 @@ function createItem(todo: TodoData) {}
 
 /**
  * 할 일을 추가합니다.
- * @param content 내용
+ * @param {string} content 내용
  */
 export function create(content: string) {}
 
@@ -25,16 +25,16 @@ export function readAll() {}
 
 /**
  * id를 기반으로 특정 할 일을 조회합니다.
- * @param id 읽어올 할 일의 id
+ * @param {number} id 읽어올 할 일의 id
  */
 export function readAt(id: number) {}
 
 /**
  * id를 기반으로 할 일을 수정합니다.
- * @param content 내용
- * @param isCompleted 완료여부
- * @param category 카테고리
- * @param tags 태그들
+ * @param {string} content 내용
+ * @param {boolean} isCompleted 완료여부
+ * @param {string} category 카테고리
+ * @param {string[]} tags 태그들
  */
 export function updateAt(
   content: string,
@@ -45,14 +45,14 @@ export function updateAt(
 
 /**
  * id를 기반으로 할 일의 태그들을 수정합니다.
- * @param id 할 일을 읽기 위한 id
- * @param tags 수정할 태그들
+ * @param {number} id 할 일을 읽기 위한 id
+ * @param {string[]} tags 수정할 태그들
  */
 export function updateTagsAt(id: number, tags: string[]) {}
 
 /**
  * id를 기반으로 할 일을 삭제합니다.
- * @param id 삭제할 할 일의 id
+ * @param {number} id 삭제할 할 일의 id
  */
 export function deleteItemAt(id: number) {}
 
@@ -63,13 +63,13 @@ export function deleteItemAll() {}
 
 /**
  * 할 일의 태그를 제거합니다.
- * @param id 할 일의 id
- * @param tag 삭제할 태그의 내용
+ * @param {number} id 할 일의 id
+ * @param {string} tag 삭제할 태그의 내용
  */
 export function deleteTagAt(id: number, tag: string) {}
 
 /**
  * id를 기반으로 조회된 할 일의 태그를 모두 제거합니다.
- * @param id 할 일의 id
+ * @param {number} id 할 일의 id
  */
 export function deleteTagAll(id: number) {}
